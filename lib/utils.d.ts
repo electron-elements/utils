5fcece5b38376f5aa7a34f12d90c5b50bda92875
Priyank Patel <priyankp390@gmail.com>
2018-04-01 20:03:01 -0400

    utils: add typescript's type defenations

---
+++ lib/utils.d.ts
@@ -0,0 +1,29 @@
+declare namespace ElectronElementsUtils {
+  interface AttributeChangeOpts {
+    addDataAttrs?: boolean,
+    handler: Function
+  }
+
+  class AttributeManager {
+    constructor(el: Node);
+    get(attributeName: string): null | string;
+    set(attributeName: string, value?: string): undefined;
+    onAttributeChange(attributes: string, opts: AttributeChangeOpts): any;
+    onAttributeChange(attributes: Array<string>, opts: AttributeChangeOpts): any;
+    getChangeHandlers(attr: string): Function[];
+    createAttrToPropBridge(attr: string): Function[];
+  }
+
+  class TemplateManager {
+    constructor();
+    add(name: string, html: string, data?: object): void;
+    update(name: string, data: object, noCache?: boolean): void;
+    has(name: string): boolean;
+    get(name: string): DocumentFragment;
+    getHTMLTemplat(name: string): string;
+    getCachedData(name: string): object;
+    deleteCachedData(name: string): void;
+    updateTemplate(name: string, html: string, data?: object): void;
+    remove(name: string): void;
+  }
+}
